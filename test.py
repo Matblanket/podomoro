4ebe337a23d2ed1539533aeab8f2a1b8160c6787
hecarim <ineedaspareone@gmail.com>
2023-03-07 09:32:03 +0530

    commmiting currently used ver

--- test.py
+++ test.py
@@ -1,41 +1,4 @@
-import curses
-import time
-krys = {
-    1: [(0, 0), (0, 1), (1, 1), (2, 0), (2, 1), (2, 2)],
-    2: [(0, 0), (0, 1), (1, 1), (2, 1), (2, 2)],
-    0: [(0, 0), (0, 2), (1, 0), (0, 1), (1, 2), (2, 0), (2, 1), (2, 2)],
-    3: [(0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 2), (2, 0), (2, 1)],
-    4: [(0, 0), (1, 0), (0, 2), (1, 1), (1, 2), (2, 2)],
-    5: [(0, 2), (0, 1), (1, 1), (2, 0), (2, 1)],
-    6: [(0, 0), (1, 0), (2, 0), (1, 1), (1, 2), (2, 1), (2, 2)],
-    7: [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],
-    8: [(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2), (2, 2)],
-    9: [(0, 0), (1, 0), (0, 1), (1, 1), (0, 2), (1, 2), (2, 2)]
-}
+import subprocess
 
-
-def main(stdscr):
-    stdscr.clear()
-    for i in range(10):
-        letter(stdscr, 2)
-        time.sleep(1)
-        stdscr.clear()
-
-
-def grid(i):
-    return krys[i]
-
-
-def letter(stdscr, i, offsetx=0):
-    for u in grid(i):
-        block(stdscr, u[0] * 3, u[1] * 3 + offsetx)
-
-
-def block(stdscr, y, x):
-    stdscr.addstr(y + 0, x, chr(61) * 3)
-    stdscr.addstr(y + 1, x, chr(61) * 3)
-    stdscr.addstr(y + 2, x, chr(61) * 3)
-    stdscr.refresh()
-
-
-curses.wrapper(main)
+some = subprocess.getoutput('yt-dlp -f bestaudio -x -o temp https://www.youtube.com/watch?v=niqC3BT9gck')
+print(some)
